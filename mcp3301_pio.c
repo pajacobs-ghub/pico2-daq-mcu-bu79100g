0a70d92ed52981ce107a3df4d5e1546c9754bddb
Peter Jacobs <peterj@mech.uq.edu.au>
2025-04-07 22:13:52 +1000

    Start with a shell of an interpreter.

---
+++ mcp3301_pio.c
@@ -0,0 +1,126 @@
+// mcp3301_pio.c
+// PIO example code (to be) mostly copied from here
+// https://github.com/raspberrypi/pico-examples/pio/spi/
+//
+// PJ 2025-04-06: simple interpreter without any pio interaction
+//
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/pio.h"
+#include "hardware/uart.h"
+#include "pico/binary_info.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define VERSION_STR "v0.2 2025-04-06 MCP3301 ADC via PIO"
+
+const uint READY_PIN = 22;
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+uint8_t override_led = 0;
+
+// For incoming serial comms
+#define NBUFA 80
+char bufA[NBUFA];
+
+int getstr(char* buf, int nbuf)
+// Read (without echo) a line of characters into the buffer,
+// stopping when we see a new-line character.
+// Returns the number of characters collected,
+// excluding the terminating null char.
+{
+    int i = 0;
+    char c;
+    uint8_t done = 0;
+    while (!done) {
+        c = getc(stdin);
+        if (c != '\n' && c != '\r' && c != '\b' && i < (nbuf-1)) {
+            // Append a normal character.
+            buf[i] = c;
+            i++;
+        }
+        if (c == '\n') {
+            done = 1;
+            buf[i] = '\0';
+        }
+        if (c == '\b' && i > 0) {
+            // Backspace.
+            i--;
+        }
+    }
+    return i;
+} // end getstr()
+
+void interpret_command(char* cmdStr)
+// A command that does not do what is expected should return a message
+// that includes the word "error".
+{
+    char* token_ptr;
+    const char* sep_tok = ", ";
+    uint8_t i;
+    // printf("DEBUG: cmdStr=%s", cmdStr);
+    if (!override_led) gpio_put(LED_PIN, 1); // To indicate start of interpreter activity.
+    switch (cmdStr[0]) {
+	case 'v':
+		printf("v %s\n", VERSION_STR);
+		break;
+	case 'L':
+		// Turn LED on or off.
+		// Turning the LED on by command overrides its use
+		// as an indicator of interpreter activity.
+		token_ptr = strtok(&cmdStr[1], sep_tok);
+		if (token_ptr) {
+			// Found some non-blank text; assume on/off value.
+			// Use just the least-significant bit.
+			i = (uint8_t) (atoi(token_ptr) & 1);
+			gpio_put(LED_PIN, i);
+			override_led = i;
+			printf("L %d\n", i);
+		} else {
+			// There was no text to give a value.
+			printf("L error: no value\n");
+		}
+		break;
+	default:
+		printf("%c error: Unknown command\n", cmdStr[0]);
+    }
+    if (!override_led) gpio_put(LED_PIN, 0); // To indicate end of interpreter activity.
+} // end interpret_command()
+
+
+int main()
+{
+    stdio_init_all();
+	uart_set_baudrate(uart0, 230400);
+    // Some information for picotool.
+    bi_decl(bi_program_description(VERSION_STR));
+    bi_decl(bi_1pin_with_name(LED_PIN, "LED output pin"));
+    bi_decl(bi_1pin_with_name(READY_PIN, "Ready/Busy# output pin"));
+    //
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+	// Flash LED twice at start up.
+	gpio_put(LED_PIN, 1); sleep_ms(500);
+	gpio_put(LED_PIN, 0); sleep_ms(500);
+	gpio_put(LED_PIN, 1); sleep_ms(500);
+	gpio_put(LED_PIN, 0);
+    gpio_init(READY_PIN);
+    gpio_set_dir(READY_PIN, GPIO_OUT);
+    gpio_put(READY_PIN, 1);
+    //
+    while (1) {
+        // Characters are not echoed as they are typed.
+        // Backspace deleting is allowed.
+        // NL (Ctrl-J) signals end of incoming string.
+        int m = getstr(bufA, NBUFA);
+        // Note that the cmd string may be of zero length,
+        // with the null character in the first place.
+        // If that is the case, do nothing with it.
+        if (m > 0) {
+            interpret_command(bufA);
+        }
+    }
+    return 0;
+}
+
